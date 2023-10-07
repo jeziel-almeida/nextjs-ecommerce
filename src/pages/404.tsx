969c429a969235ddf6a01f51d2c61155f616acbb
Jeziel Almeida <jeziel.almeida16@gmail.com>
2023-10-07 10:09:42 -0300

    feat: Add Error Image

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -11,11 +11,11 @@ const NotFound = () => {
         </Head>
 
         <Layout>
-            <div className='w-full h-full flex flex-col justify-center items-center'>
-                <Image className='mb-4' src={'/favicon.ico'} width={25} height={25} alt='Duas pessoas tentando consertar um servidor quebrado' />
+            <div className='w-full min-h-[22rem] flex flex-col justify-center items-center'>
+                <Image className='mb-4' src={'/404Error.svg'} width={200} height={200} alt='Duas pessoas tentando consertar um servidor quebrado' />
                 <h1 className='text-3xl font-bold mb-4'>OOPS!</h1>
                 <p className='text-lg mb-4'>Há algo errado com o nosso servidor. Estamos tentando consertá-lo. Por favor, tente novamente mais tarde.</p>
-                <button className='btn btn-secondary'>Tentar novamente</button>
+                <button onClick={() => {location.reload()}} className='btn btn-secondary'>Tentar novamente</button>
             </div>
         </Layout>
     </>
